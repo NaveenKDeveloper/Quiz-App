edfa5981326365f3bc504c10135b418df734058b
Naveen K <naveen8972@gmail.com>
2021-10-09 11:31:44 +0530

    adding state

--- src/context.js
+++ src/context.js
@@ -11,12 +11,21 @@ const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
 const url = ''
 
+const temp= 'https://opentdb.com/api.php?amount=15&category=27&difficulty=easy&type=multiple'
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+
+  const [wait,setWait]=useState(true)
+  const [load,setLoad]=useState(false)
+  const [questions,setQuestions]=useState([])
+  const [index,setIndex]=useState(0)
+  const [correct,setCorrect]=useState(0)
+  const [error,setError]=useState(0)
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
