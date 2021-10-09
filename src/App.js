03695359ca27aed9462732c1f416f6f62838daf4
Naveen K <naveen8972@gmail.com>
2021-10-09 14:57:24 +0530

    adding display

--- src/App.js
+++ src/App.js
@@ -14,10 +14,35 @@ function App() {
   if(load){
     return <Loading />
   }
-
-  return <main>
-    Quiz data
-  </main>
+  const {question,incorrect_answer,correct_answer}=questions[0]
+  const answers = [...incorrect_answer,correct_answer]
+  return (<main>
+    <Modal />
+    <section className='quiz'>
+      <p className='correct-answers'>
+       Correct Answer : {correct}/{index}
+      </p>
+      <article className='container'>
+        <h2 dangerouslySetInnerHTML={{__html: question}}/>
+        <div className='btn-container'>
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  onClick={() => checkAnswer(correct_answer === answer)}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              )
+            })}
+          </div>
+      </article>
+      <button className='next-question' onClick={nextQuestion}>
+          Next Question
+        </button>
+      
+    </section>
+  </main> )
 
 }
 
